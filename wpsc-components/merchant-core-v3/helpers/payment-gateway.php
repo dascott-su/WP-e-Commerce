60a75c6a4bf5077647c362351ca79560fc3dda36
Justin Sainton <justinsainton@gmail.com>
2016-12-05 14:33:51 -0800

    Better support for gateways that are not properly registered.

--- wpsc-components/merchant-core-v3/helpers/payment-gateway.php
+++ wpsc-components/merchant-core-v3/helpers/payment-gateway.php
@@ -24,6 +24,7 @@ function wpsc_payment_gateway_supports( $gateway, $supports ) {
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 	return $supports;
 }
 =======
@@ -32,5 +33,8 @@ function wpsc_payment_gateway_supports( $gateway, $supports ) {
 
 >>>>>>> 3a7a7542... Update docs and data type for WPSC_Payment_Gateways::get().
 	return $support;
+=======
+	return wpsc_get_payment_gateway( $gateway )->supports( $supports );
+>>>>>>> e68ad0e9... Better support for gateways that are not properly registered.
 }
 >>>>>>> 389d1b93... Add spinner feedback to refund UI.
