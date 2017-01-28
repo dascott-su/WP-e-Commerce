e68c29196d0cc504527edb60ed976b43ccfc0a91
Justin Sainton <justinsainton@gmail.com>
2017-01-27 17:17:15 -0800

    Merge original wpsc_tracking commit.

--- wpsc-core/wpsc-includes.php
+++ wpsc-core/wpsc-includes.php
@@ -80,3 +80,11 @@ if ( is_admin() ) {
 
 // Cron
 require_once( WPSC_FILE_PATH . '/wpsc-includes/cron.php' );
+
+// WP-CLI support
+if ( defined( 'WP_CLI' ) && WP_CLI && version_compare( phpversion(), '5.3', '>=' ) ) {
+	require_once( WPSC_FILE_PATH . '/wpsc-includes/wpsc-wp-cli.php' );
+}
+
+// Tracking
+require_once( WPSC_FILE_PATH . '/wpsc-includes/wpsc-tracking.php' );
