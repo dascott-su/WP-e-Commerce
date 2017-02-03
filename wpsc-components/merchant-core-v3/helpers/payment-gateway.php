72bfe9ee3dc681196e129fc4a20220fb87323b8d
Joldis Mihai Alexandru <jmihaialexandru@gmail.com>
2017-02-03 23:47:00 +0200

    Updates

--- wpsc-components/merchant-core-v3/helpers/payment-gateway.php
+++ wpsc-components/merchant-core-v3/helpers/payment-gateway.php
@@ -22,19 +22,5 @@ function wpsc_payment_gateway_supports( $gateway, $supports ) {
 		return false;
 	}
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-	return $supports;
-}
-=======
-=======
-	$support = $gateway->supports( $supports );
-
->>>>>>> 3a7a7542... Update docs and data type for WPSC_Payment_Gateways::get().
-	return $support;
-=======
 	return wpsc_get_payment_gateway( $gateway )->supports( $supports );
->>>>>>> e68ad0e9... Better support for gateways that are not properly registered.
 }
->>>>>>> 389d1b93... Add spinner feedback to refund UI.
